69a254fe8095158b7dc705e825a6c93f65bcbf7d
xCoD3Rx <76232388+xCoD3Rx@users.noreply.github.com>
2021-07-11 17:49:47 +0300

    Add files via upload

---
+++ KonuBot/KonuBot/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace KonuBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 255);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(896, 230);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.Url = new System.Uri("http://www.cyber-warrior.org/forum/web-guvenlik-aciklari-13,0.cwx", System.UriKind.Absolute);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(0, 10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(269, 116);
+            this.listBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(496, 10);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(392, 127);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Kaçıncı Linkten Başlasın:";
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(0, 255);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.ScriptErrorsSuppressed = true;
+            this.webBrowser2.Size = new System.Drawing.Size(896, 230);
+            this.webBrowser2.TabIndex = 11;
+            this.webBrowser2.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Devam Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Konuyu Açacağınız Yerin Linki:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(525, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(313, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Text = "http://www.sefalet.net/Forum.php/board,19.0.html";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(330, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Konuları Çekeceğiniz Bölümün Linki:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(525, 186);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(313, 20);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.Text = "http://www.cyber-warrior.org/Forum/mail-guvenlik-aciklari-15,0.cwx";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(386, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Giriş Yap";
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 3000;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(346, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Durdur";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer6
+            // 
+            this.timer6.Interval = 10;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.HorizontalScrollbar = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(0, 132);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.ScrollAlwaysVisible = true;
+            this.listBox2.Size = new System.Drawing.Size(269, 116);
+            this.listBox2.TabIndex = 20;
+            this.listBox2.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(279, 109);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(127, 17);
+            this.radioButton1.TabIndex = 21;
+            this.radioButton1.Text = "Otomatik Paylaşım Aç";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(412, 109);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 22;
+            this.radioButton2.Text = "/Açma";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 480);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer6;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ KonuBot/KonuBot/Form1.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace KonuBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void linklericek()
+        {
+          
+            textBox1.Clear();
+            webBrowser1.Navigate(textBox4.Text);
+            webBrowser2.Navigate(textBox3.Text);
+            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            if (textBox2.Text != "")
+            {
+
+
+                while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                {
+                    Application.DoEvents();
+                }
+                HtmlElementCollection links = webBrowser1.Document.GetElementsByTagName("a");
+
+                foreach (HtmlElement item in links)
+                {
+
+                    try
+                    {
+                        string id = item.GetAttribute("href");
+                        // string[] deger = id.Split('=');
+                        int index = id.LastIndexOf(",0.cwx");
+                        
+                        if (index != -1)
+                        {
+                            
+                            listBox1.Items.Add(id);
+                            this.Text = listBox1.Items.Count.ToString();
+                        }
+                    }
+                    catch
+                    {
+
+
+                    }
+
+                }
+
+                timer1.Enabled = true;
+            }
+            else { MessageBox.Show("Başlanacak Link Sayısını Girin"); }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string a;
+            HtmlElementCollection col = webBrowser1.Document.GetElementsByTagName("td");
+            foreach (HtmlElement item in col)
+            {
+
+                a = item.InnerText;
+                MessageBox.Show(a);
+
+            }
+         
+            }
+
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= listBox1.Items.Count-1; i++)
+            {for (int j = 0; j <= 10; j++)
+			{
+                try
+                {
+                    if (listBox1.Items[i].ToString() == j.ToString())
+                    {
+                        listBox1.Items.RemoveAt(i);
+                    }
+                }
+                catch 
+                {
+                    
+                  
+                }
+			 
+			}
+                
+            }
+            timer1.Enabled = true;
+        }
+        
+        int adet;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            while (webBrowser2.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            textBox1.Clear();
+            if (textBox2.Text == "")
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Kutu Boş olamaz");
+
+            }
+            else
+            {
+                adet = Convert.ToInt32(textBox2.Text);
+                adet = adet + 1;
+                textBox2.Text = (adet).ToString();
+
+
+                timer1.Enabled = false;
+
+                {
+                    webBrowser1.Navigate(listBox1.Items[adet].ToString());
+                    baslık = listBox1.Items[adet].ToString();
+                    int sayi = baslık.Length;
+                    baslık = baslık.Substring(35, sayi - 48);
+                    baslık2 = baslık.Split('-');
+                    baslık = "";
+                    for (int i = 0; i <= baslık2.Length - 1; i++)
+                    {
+                        baslık += " " + baslık2[i].ToString();
+                    }
+                    timer2.Enabled = true;
+
+
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            HtmlElementCollection linkler = webBrowser1.Document.GetElementsByTagName("a");
+
+            foreach (HtmlElement element in linkler)
+            {
+                listBox1.Items.Add(element.InnerHtml + "    " + element.InnerText);
+                //        MessageBox.Show(element.InnerHtml+ "    "+ element.InnerText);
+            }
+
+        }
+       
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            linklericek();
+        }
+        string baslık;
+        string []baslık2;
+        
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            int adet = 0;
+          
+            timer2.Enabled = false;
+            while (webBrowser1.ReadyState!=WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            while (webBrowser2.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            linkcek();
+            foreach (HtmlElement elm in webBrowser1.Document.All)
+            {
+                if (elm.GetAttribute("className") == "mesaj")
+                {
+                    //    degerler.Add(elm.InnerText);
+                 adet++;
+                
+                        textBox1.Text += (elm.InnerText);
+                  
+            
+
+
+                    if (adet==1)
+                    {
+                        for (int i = 0; i <= listBox2.Items.Count; i++)
+                        {
+                            try
+                            {
+                               // textBox1.Text += "KONUYA AİT LİNKLER:" + "[url]" + listBox2.Items[i].ToString() + "[/url]";
+                            }
+                            catch 
+                            {
+                                
+                             
+                            }
+
+                           
+                            
+                        }
+                    }
+                 
+                       
+                        
+                    
+                }
+            }
+            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            listBox2.Items.Clear();
+            webBrowser2.Navigate(textBox3.Text);
+            while (webBrowser2.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            timer3.Enabled = true;
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+            foreach (HtmlElement elm in webBrowser2.Document.All)
+            {
+                if (elm.GetAttribute("alt") == "Yeni Konu")
+                {
+                    elm.InvokeMember("click");
+                }
+            }
+            while (webBrowser2.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            timer4.Enabled = true;
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            timer4.Enabled = false;
+        
+
+            foreach (HtmlElement elm in webBrowser2.Document.All)
+            {
+                if (elm.GetAttribute("name") == "subject")
+                {
+                    elm.InnerText = baslık;
+                }
+            }
+            foreach (HtmlElement elm in webBrowser2.Document.All)
+            {
+                if (elm.GetAttribute("id") == "message")
+                {
+                    elm.InnerText = textBox1.Text;
+                }
+            }
+            timer6.Enabled = true;
+          //  devamet();
+
+        }
+        public void devamet()
+        {
+            foreach (HtmlElement elm in webBrowser2.Document.All)
+            {
+                if (elm.GetAttribute("value") == "Gönder")
+                {
+                    elm.InvokeMember("Click");
+                }
+            }
+
+            while (webBrowser2.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+           // webBrowser2.Navigate(textBox3.Text);
+        }
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            devamet();
+            timer1.Enabled = true;
+        }
+
+   
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+            webBrowser2.Navigate(textBox3.Text);
+          
+          
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            timer5.Enabled = false;
+            devamet();
+            timer1.Enabled = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer6.Enabled = true;
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked==true)
+            {
+                timer6.Enabled = false;
+
+                timer5.Enabled = true;
+
+            }
+            else if (radioButton2.Checked)
+            {
+                timer6.Enabled = false;
+                timer5.Enabled = false;
+            }
+            else
+            {
+                timer6.Enabled = false;
+            }
+        }
+        int[] index;
+        string[] ab;
+        int sayi;
+        public void linkcek()
+        {
+
+            ab = new string[14];
+            index = new int[7];
+            ab[0] = "http://www.cyber";
+            ab[1] = "http://www.grafiklerdunyasi.com";
+
+            ab[2] = "http://www.facebook.com";
+            ab[3] = "http://www.bilgininadresi.net";
+            ab[4] = "http://www.bg.org.tr";
+            ab[5] = "http://www.turk-h.org";
+            ab[6] = "http://www.kemalsener.av.tr";
+            ab[7] = "http://www.adlibilirkisi.org";
+            ab[8] = "http://www.bilenesor.org";
+            ab[9] = "javascript";
+            ab[10] = "msnim";
+            ab[11] = " ";
+            ab[12] = "http://cyber-security.org.tr";
+            ab[13] = "http://www.grafikerlerdunyasi.com";
+
+
+
+
+
+            HtmlElementCollection links = webBrowser1.Document.GetElementsByTagName("a");
+
+            foreach (HtmlElement item in links)
+            {
+
+                try
+                {
+                    string id = item.GetAttribute("href");
+                    sayi = 0;
+
+                    int index2 = id.IndexOf(ab[0]);
+                    int index3 = id.IndexOf(ab[1]);
+                    int index4 = id.IndexOf(ab[2]);
+                    int index5 = id.IndexOf(ab[3]);
+                    int index6 = id.IndexOf(ab[4]);
+                    int index7 = id.IndexOf(ab[5]);
+                    int index8 = id.IndexOf(ab[6]);
+                    int index9 = id.IndexOf(ab[7]);
+                    int index10 = id.IndexOf(ab[8]);
+                    int index11 = id.IndexOf(ab[9]);
+                    int index12 = id.IndexOf(ab[10]);
+                    int index13 = id.IndexOf(ab[11]);
+                    int index14 = id.IndexOf(ab[12]);
+                    int index15 = id.IndexOf(ab[13]);
+                    if (index2 == -1 & index3 == -1 & index4 == -1 & index5 == -1 & index6 == -1 & index7 == -1 & index8 == -1 & index9 == -1 & index10 == -1 & index11 == -1 & index12 == -1 & index13 == -1 & index14 == -1 & index15 == -1)
+                    {
+                        string yazı;
+                        yazı = id;
+                        string[] yazı2;
+                        char[] karakter = { ' ' };
+                        yazı2 = yazı.Split(karakter, StringSplitOptions.RemoveEmptyEntries);
+                        yazı = "";
+                      
+                        for (int i = 0; i <= yazı2.Length - 1; i++)
+                        {
+
+                            yazı += yazı2[i];
+                            
+                         
+                                listBox2.Items.Add(yazı);
+  
+                        
+                        }
+                        
+                    
+                    }
+
+                }
+                catch
+                {
+
+
+                }
+            }
+        }
+
+
+      
+
+     
+        }
+    }
+
+
